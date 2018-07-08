1c7403711870929b2c4bb06b067bc9cda0f01cd0
Eoin <eoin4real@gmail.com>
2018-07-08 18:49:39 -0400

    Remove streaming route

--- src/main/java/ie/eoin/sample/ojos/resources/ImageResource.java
+++ src/main/java/ie/eoin/sample/ojos/resources/ImageResource.java
@@ -6,10 +6,7 @@ import java.net.URI;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.imageio.ImageIO;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -42,23 +39,13 @@ public class ImageResource {
     return Response.seeOther(new URI(response.getImageLocation())).build();
   }
 
-  // TODO Exception Handling
-  @Path("/default")
-  @GET
-  @Produces("image/png")
-  public Response getDefaultImage() throws Exception {
-
-    BufferedImage image =
-        ImageIO.read(ImageResource.class.getResourceAsStream("/assets/images/1x1.png"));
-
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    ImageIO.write(image, "png", baos);
-    byte[] imageData = baos.toByteArray();
-
-    // uncomment line below to send non-streamed
-    return Response.ok(imageData).build();
-
-    // uncomment line below to send streamed
-    //        return Response.ok(new ByteArrayInputStream(imageData)).build();
+  @Path("/info")
+  @POST
+  public Response getRedirectResponse(
+          @QueryParam("url") String url, @QueryParam("selector") String selector) throws Exception {
+    ImageResponse response = capturaClient.getImage(new ImageRequest(url, selector));
+    return Response.seeOther(new URI(response.getImageLocation())).build();
   }
+
+
 }
